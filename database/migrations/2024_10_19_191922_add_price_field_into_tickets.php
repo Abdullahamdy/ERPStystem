5bc1abd9ba1ee840501912afcc4e7f4b3664a24c
abdullahalawady <abdullah_hamdy@grand-co>
2024-11-04 16:46:14 +0200

    add ticket all

--- database/migrations/2024_10_19_191922_add_price_field_into_tickets.php
+++ database/migrations/2024_10_19_191922_add_price_field_into_tickets.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddProductIdFieldIntoPrinters extends Migration
+class AddPriceFieldIntoTickets extends Migration
 {
     /**
      * Run the migrations.
