94542d9ec7633ff5084c907eb15d045643ab823f
abdullahalawady <abdullah_hamdy@grand-co>
2024-11-17 02:08:10 +0200

    handle select ticket

---
+++ database/migrations/2024_11_17_053535_add_bussiness_id_in_to__printers.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddBussinessIdInToPrinters extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('printers', function (Blueprint $table) {
+            $table->integer('business_id')->unsigned();
+            $table->foreign('business_id')->references('id')->on('business')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('printers', function (Blueprint $table) {
+            //
+        });
+    }
+}
